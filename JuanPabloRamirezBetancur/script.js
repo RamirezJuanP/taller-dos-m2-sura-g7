597de64b4d4bbb1698aceaffd97f0a5f98df1111
RamirezJuanP <juanpablorb2000@gmail.com>
2025-11-21 10:10:49 -0500

    Resolviendo el segundo taller ejercicio 1

---
+++ JuanPabloRamirezBetancur/script.js
@@ -0,0 +1,21 @@
+/*  Ejercicio 1: Duplicar Números (map)
+Inicias con una lista de puntos:
+let puntos = [10, 20, 30, 40];
+
+Tareas:
+1.  Crea un nuevo arreglo puntosDoble donde cada valor sea el doble del arreglo original.
+2.  Muestra puntos y puntosDoble para verificar que el original no cambió.
+
+---
+   */
+
+let puntos = [10, 20, 30, 40];
+
+
+let puntosDoble = puntos.map(function(punto) {
+    return punto * 2;
+});
+
+
+console.log(puntos);
+console.log(puntosDoble);
