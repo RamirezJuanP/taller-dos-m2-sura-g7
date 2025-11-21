f1bb6a0d9110f65e8699a7ddf1ca4dfe8d649f2e
RamirezJuanP <juanpablorb2000@gmail.com>
2025-11-21 10:24:52 -0500

    Resolviendo el segundo taller ejercicio 3

--- JuanPabloRamirezBetancur/script.js
+++ JuanPabloRamirezBetancur/script.js
@@ -19,3 +19,54 @@ let puntosDoble = puntos.map(function(punto) {
 
 console.log(puntos);
 console.log(puntosDoble);
+
+
+/* 
+  Ejercicio 2: Lista de Nombres (map con objetos)
+Inicias con una lista de usuarios:
+let usuarios = [{id: 1, nombre: "Ana"}, {id: 2, nombre: "Luis"}, {id: 3, nombre: "Carlos"}];
+
+Tareas:
+1.  Crea un nuevo arreglo nombres que contenga solo los nombres de los usuarios (ej: ["Ana", "Luis", "Carlos"]).
+
+--- 
+ */
+
+let usuarioss = [
+    { id: 1, nombre: "Ana" },
+    { id: 2, nombre: "Luis" },
+    { id: 3, nombre: "Carlos" },
+];
+
+// Crea un nuevo arreglo nombres que contenga solo los nombres de los usuarios (ej: ["Ana", "Luis", "Carlos"]).
+
+let nombres = usuarioss.map((usuario) => {
+    return usuario.nombre;
+});
+
+console.log(usuarioss);
+console.log(nombres);
+
+
+
+
+
+/*    Ejercicio 3: Notas Aprobadas (filter)
+Inicias con una lista de calificaciones:
+let notas = [8, 5, 10, 3, 7];
+
+Tareas:
+1.  Crea un nuevo arreglo aprobadas que contenga solo las notas que sean mayores o iguales a 6.
+
+---
+  */
+
+let notas = [8, 5, 10, 3, 7];
+
+// Crea un nuevo arreglo aprobadas que contenga solo las notas que sean mayores o iguales a 6.
+
+let aprobadas = notas.filter((nota) => nota >= 6);
+
+console.log(notas);
+console.log(aprobadas);
+
